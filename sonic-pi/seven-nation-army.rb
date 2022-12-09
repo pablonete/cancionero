d720a8b9f93c2bee1dd0f6996afc8cba29bae57f
Pablo Núñez <pablonete@github.com>
2022-12-09 13:57:41 +0100

    Update seven-nation-army.rb

--- sonic-pi/seven-nation-army.rb
+++ sonic-pi/seven-nation-army.rb
@@ -1,1 +1,13 @@
 # Seven Nation Army by OpenAI
+
+use_synth :tb303
+with_fx :distortion do
+  play :C3, release: 8, attack: 1, cutoff: 80
+  sleep 0.5
+  play :C3, release: 8, attack: 1, cutoff: 80
+  sleep 0.5
+  play :C3, release: 8, attack: 1, cutoff: 80
+  sleep 0.5
+  play :G2, release: 8, attack: 1, cutoff: 80
+  sleep 0.5
+end
