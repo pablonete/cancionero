a2d81acc79cb8cab05bc6e64ef73d985220b331b
Pablo Núñez <pablonete@github.com>
2022-12-09 11:58:56 +0100

    Arreglar duraciones, añadir loop

--- sonic-pi/cumpleaños-feliz.rb
+++ sonic-pi/cumpleaños-feliz.rb
@@ -1,56 +1,59 @@
 # Cumpleaños feliz
 
-play :C4
-sleep 0.5
-play :C4
-sleep 0.5
-play :D4
-sleep 0.5
-play :C4
-sleep 0.5
-play :F4
-sleep 0.5
-play :E4
-sleep 1
-
-play :C4
-sleep 0.5
-play :C4
-sleep 0.5
-play :D4
-sleep 0.5
-play :C4
-sleep 0.5
-play :G4
-sleep 0.5
-play :F4
-sleep 1
-
-play :C4
-sleep 0.5
-play :C4
-sleep 0.5
-play :C5
-sleep 0.5
-play :A4
-sleep 0.5
-play :F4
-sleep 0.5
-play :E4
-sleep 0.5
-play :D4
-sleep 1
-
-play :Bb4
-sleep 0.5
-play :Bb4
-sleep 0.5
-play :A4
-sleep 0.5
-play :F4
-sleep 0.5
-play :G4
-sleep 0.5
-play :F4
-sleep 0.5
-
+live_loop :melody do
+  use_bpm 100
+  
+  play :C4
+  sleep 0.5
+  play :C4
+  sleep 0.5
+  play :D4
+  sleep 1
+  play :C4
+  sleep 1
+  play :F4
+  sleep 1
+  play :E4
+  sleep 2
+  
+  play :C4
+  sleep 0.5
+  play :C4
+  sleep 0.5
+  play :D4
+  sleep 1
+  play :C4
+  sleep 1
+  play :G4
+  sleep 1
+  play :F4
+  sleep 2
+  
+  play :C4
+  sleep 0.5
+  play :C4
+  sleep 0.5
+  play :C5
+  sleep 1
+  play :A4
+  sleep 1
+  play :F4
+  sleep 1
+  play :E4
+  sleep 1
+  play :D4
+  sleep 2
+  
+  play :Bb4
+  sleep 0.5
+  play :Bb4
+  sleep 0.5
+  play :A4
+  sleep 1
+  play :F4
+  sleep 1
+  play :G4
+  sleep 1
+  play :F4
+  sleep 2
+end
