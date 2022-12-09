2952a24459e4420e26dbbe07442b0959aa9d00fb
Pablo Núñez <pablonete@github.com>
2022-12-09 11:15:05 +0100

    Primeros 2 versos

---
+++ sonic-pi/cumpleaños-feliz.rb
@@ -0,0 +1,26 @@
+# Cumpleaños feliz
+
+play :C
+sleep 0.5
+play :C
+sleep 0.5
+play :D
+sleep 0.5
+play :C
+sleep 0.5
+play :F
+sleep 0.5
+play :E
+sleep 1
+
+play :C
+sleep 0.5
+play :C
+sleep 0.5
+play :D
+sleep 0.5
+play :C
+sleep 0.5
+play :G
+sleep 0.5
+play :F
