4c294888c1667377009e80d84704ad9c37c2d89a
Pablo Núñez <pablonete@github.com>
2022-12-09 12:25:53 +0100

    Usar escala y loops para toda la melodía

--- sonic-pi/cumpleaños-feliz.rb
+++ sonic-pi/cumpleaños-feliz.rb
@@ -1,61 +1,45 @@
 # Cumpleaños feliz
 
-escala = [:C4, :D4, :E4, :F4]
+escala = [:C4, :D4, :E4, :F4, :G4, :A4, :C5]
 
 live_loop :melody do
   use_bpm 100
   
-  play escala[0]
-  sleep 0.5
-  play escala[0]
-  sleep 0.5
-  play escala[1]
+  2.times do |i|
+    2.times do
+      play escala[0]
+      sleep 0.5
+    end
+    play escala[1]
+    sleep 1
+    play escala[0]
+    sleep 1
+    play escala[3+i]
+    sleep 1
+    play escala[2+i]
+    sleep 2
+  end
+  
+  2.times do
+    play escala[0]
+    sleep 0.5
+  end
+  escala.reverse.reject {|n| n == :G4}.take(5).map do |note|
+    play note
+    sleep 1
+  end
   sleep 1
-  play escala[0]
+  
+  2.times do
+    play escala[5]+1
+    sleep 0.5
+  end
+  play escala[5]
   sleep 1
   play escala[3]
   sleep 1
-  play escala[2]
-  sleep 2
-  
-  play :C4
-  sleep 0.5
-  play :C4
-  sleep 0.5
-  play :D4
+  play escala[4]
   sleep 1
-  play :C4
-  sleep 1
-  play :G4
-  sleep 1
-  play :F4
-  sleep 2
-  
-  play :C4
-  sleep 0.5
-  play :C4
-  sleep 0.5
-  play :C5
-  sleep 1
-  play :A4
-  sleep 1
-  play :F4
-  sleep 1
-  play :E4
-  sleep 1
-  play :D4
-  sleep 2
-  
-  play :Bb4
-  sleep 0.5
-  play :Bb4
-  sleep 0.5
-  play :A4
-  sleep 1
-  play :F4
-  sleep 1
-  play :G4
-  sleep 1
-  play :F4
+  play escala[3]
   sleep 2
 end
