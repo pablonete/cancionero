a4fecf71b53f5c6c2a67df9f35b4b5d2a11bc9ff
Pablo Núñez <pablonete@github.com>
2022-12-09 16:18:34 +0100

    Simplificar con play_pattern_timed

--- sonic-pi/cumpleaños-feliz.rb
+++ sonic-pi/cumpleaños-feliz.rb
@@ -2,7 +2,7 @@
 
 escala = [:C4, :D4, :E4, :F4, :G4, :A4, :C5]
 
-use_bpm 100
+use_bpm 120
 
 live_loop :kick do
   sample :bd_haus
@@ -14,42 +14,19 @@ end
 live_loop :melody do
   use_synth :piano
   
-  2.times do |i|
-    2.times do
-      play escala[0]
-      sleep 0.5
-    end
-    play escala[1]
-    sleep 1
-    play escala[0]
-    sleep 1
-    play escala[3+i]
-    sleep 1
-    play escala[2+i]
-    sleep 2
-  end
-  
-  2.times do
-    play escala[0]
-    sleep 0.5
-  end
-  escala.reverse.reject {|n| n == :G4}.take(5).map do |note|
-    play note
-    sleep 1
-  end
+  play_pattern_timed [:c4, :c4], 0.5
+  play_pattern_timed [:d4, :c4, :f4, :e4], 1
   sleep 1
   
-  2.times do
-    play escala[5]+1
-    sleep 0.5
-  end
-  play escala[5]
+  play_pattern_timed [:c4, :c4], 0.5
+  play_pattern_timed [:d4, :c4, :g4, :f4], 1
   sleep 1
-  play escala[3]
+  
+  play_pattern_timed [:c4, :c4], 0.5
+  play_pattern_timed [:c5, :a4, :f4, :e4, :d4], 1
   sleep 1
-  play escala[4]
+  
+  play_pattern_timed [:bb4, :bb4], 0.5
+  play_pattern_timed [:a4, :f4, :g4, :f4], 1
   sleep 1
-  play escala[3]
-  sleep 2
 end
-
